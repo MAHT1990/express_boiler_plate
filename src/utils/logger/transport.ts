109e1d409b215b049e7a0ee52ccdb9064d7aeeed
Marotik <kitoram3@gmail.com>
2025-02-12 13:02:35 +0900

    [MODIFY].env.sample modification

--- src/utils/logger/transport.ts
+++ src/utils/logger/transport.ts
@@ -1,18 +1,18 @@
 /* THIRD-PARTY MODULES */
-import { Transport } from "winston-transport";
+import TransportStream from "winston-transport";
 
 
 /**
  * 커스텀 로그 전송 클래스
  * - Winston 로그 전송 클래스를 상속받아 커스텀 로그 전송 기능을 구현
  */
-export default class CustomTransport extends Transport {
+export default class CustomTransport extends TransportStream {
 
-    constructor(opts) {
+    constructor(opts?: TransportStream.TransportStreamOptions) {
         super(opts);
     }
 
-    public async log(info, callback) {
+    public async log(info: any, callback: () => void): Promise<void> {
         setImmediate(() => {
             this.emit("logged", info);
         });
